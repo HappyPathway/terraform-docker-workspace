c38d9886fad6d0eb67ccbe16de0ad313d5132817
Dave Arnold <dave@roknsound.com>
2024-09-09 12:17:59 -0700

    refactor: Update main.tf to use "gh_" prefix for GitHub Actions variables

--- main.tf
+++ main.tf
@@ -25,8 +25,8 @@ module "docker-workspace" {
   # Combine GitHub Actions variables with other variables
   vars = concat([
     for _var, _val in var.github_actions : {
-      name  = "github_${_var}" # Name of the GitHub Actions variable
-      value = _val             # Value of the GitHub Actions variable
+      name  = "gh_${_var}" # Name of the GitHub Actions variable
+      value = _val         # Value of the GitHub Actions variable
     }
     ],
     var.vars # Additional variables
